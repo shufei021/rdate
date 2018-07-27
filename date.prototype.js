82b10f5b7abba88966675ef5ff1cfdbee304fcae
舒飞 <1017981699@qq.com>
2018-07-27 21:48:42 +0800

    Update date.prototype.js

--- date.prototype.js
+++ date.prototype.js
@@ -303,7 +303,7 @@ Date.prototype.dateFormat=function(format){
 	  "S+": t.getMilliseconds()
 	};
 	if (/(y+)/i.test(format)) {
-	    format = format.replace(RegExp.$1, (d.getFullYear() + '').substr(4 - RegExp.$1.length));
+	    format = format.replace(RegExp.$1, (t.getFullYear() + '').substr(4 - RegExp.$1.length));
 	}
 	for (var k in date) {
 		if (new RegExp("(" + k + ")").test(format)) {
