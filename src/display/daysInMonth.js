6bd2c825ee992e92a5d3261007ca6bd70aa6539b
Free-Fly <1017981699@qq.com>
2021-07-04 02:01:29 +0000

    feat：update notes

--- src/display/daysInMonth.js
+++ src/display/daysInMonth.js
@@ -8,3 +8,30 @@ const daysInMonth = function (d = new Date()) {
     return endOf('M', new Date(d), 'D')
 }
 export default daysInMonth
+
+
+/*
+const getMonthDays = (m, d = new Date()) => {
+  // parse date-time
+  const dt = new Date(d.toString())
+  // get full year
+  const year = dt.getFullYear()
+  // get month
+  const month = m === undefined ? dt.getMonth() + 1 : m
+  // get days in month
+  return new Date(year, month, 0).getDate()
+}
+
+// get the number of days in any month of this year  
+
+getMonthDays(1)
+
+getMonthDays(12)
+
+// get the number of days in any month of the passed date 
+
+getMonthDays(2,2021) // 28
+
+getMonthDays(2,2000) // 29
+
+*/
